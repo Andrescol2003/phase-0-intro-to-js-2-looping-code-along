3adad6b2a665ed674df3d80927908df5d8c147b6
andres <andres.nyc.col@gmail.com>
2024-11-04 19:08:44 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]; /* calling the arrys names*/
+
+function writeCards(names) {
+    const messages = []; /* this will tell our new const taht the message need to comes as ab array*/
+    for (let i = 0; i < names.length; i++) {messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);}
+    return messages;
+}
+
+console.log(writeCards(names));
+
+
+function countDown(start) {
+    while (start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
+countDown(11); 
